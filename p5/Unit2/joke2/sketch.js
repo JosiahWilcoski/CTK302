d84bc13d86c41ff460e2ee83fc84e4117b73521d
JosiahWilcoski <58442703+JosiahWilcoski@users.noreply.github.com>
2020-10-04 14:43:16 -0500

    how much longer must I suffer

--- p5/Unit2/joke2/sketch.js
+++ p5/Unit2/joke2/sketch.js
@@ -15,25 +15,21 @@ function draw() {
     case 0:
     background('yellow') ;
     text("What's the best part of switzerland?", 250, 250, 350, 400) ;
-
-    timer++ ;
-
-    if(time > 200) {
-      state = 1 ;
-      timer = 0 ;
+    timer = timer + 1;
+    if (timer > 3*60) {
+      state = 1;
+      timer = 0;
     }
     break ;
 
     case 1:
     background('blue') ;
     text("Well the flag is a big plus", 250, 250, 350, 400) ;
-
-        timer = timer + 1 ;
-
-        if(time > 30) {
-          state = 0 ;
-          timer = 0 ;
-        }
+    timer = timer + 1;
+    if (timer > 3*60) {
+      state = 0;
+      timer = 0;
+    }
     break ;
 
   }
